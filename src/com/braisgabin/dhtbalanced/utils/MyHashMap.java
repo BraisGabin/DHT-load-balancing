21a28bf6e0d9e4193599e72ca800614942a2395f
Brais Gabín <brais.gabin@inqbarna.com>
2013-06-12 19:05:01 +0200

    Fix warnning

--- src/com/braisgabin/dhtbalanced/utils/MyHashMap.java
+++ src/com/braisgabin/dhtbalanced/utils/MyHashMap.java
@@ -4,9 +4,10 @@ import java.util.HashMap;
 import java.util.Observer;
 
 public class MyHashMap<K, V> extends HashMap<K, V> {
+	private static final long serialVersionUID = 1L;
 
 	private Observer ob;
-	
+
 	public void setObserver(Observer ob) {
 		this.ob = ob;
 	}
